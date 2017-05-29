64bff66ad2f744a76826540ea4ba1c7f693a7077
James Block <james@hewines.com>
2017-05-29 16:27:05 +0100

    adding devise to our app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,9 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
+   #  defined default url options for mailer for production
+  config.action_mailer.default_url_options = { host: 'https://portfolio-hub.herokuapp.com' }
+
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "portfolio_#{Rails.env}"
