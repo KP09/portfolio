a7c4af9519242646de72b584c764b7f80250a53a
Kees Postma <kk.postma@gmail.com>
2017-05-31 12:19:37 +0100

    Enables attachinary in the app

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  # Model associations
   has_many :projects
   has_many :participations
   has_many :contributions, through: :participations
+
+  # User validations
+  validates :email, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :bio, presence: true
+
+  # Attachinary associations
+  has_attachment :profile_picture
+  has_attachment :cover_photo
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  mount Attachinary::Engine => "/attachinary"
 
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit, :update]
 
   resources :projects do
     resources :participations, only: [:create, :destroy]
