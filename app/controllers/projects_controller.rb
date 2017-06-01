f11e5f9348613152c7c0b44b82f4c445f2a785b3
James Block <james@hewines.com>
2017-06-01 18:07:20 +0100

    end of day - changes to show page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -30,9 +30,13 @@ class ProjectsController < ApplicationController
   end
 
   def edit
+    @project = Project.find(params[:id])
   end
 
   def update
+    @project = Project.find(params[:id])
+    @project.update(project_params)
+    redirect_to project_path(@project)
   end
 
   def destroy
