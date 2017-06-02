581c93d6afaf1bb46be68ee54d362dbe17687071
James Block <james@hewines.com>
2017-06-02 16:27:28 +0100

    changes

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -1,8 +1,6 @@
 class Contribution < ApplicationRecord
   mount_uploader :file, ContributionUploader
   belongs_to :participation
-  has_one :project, through: :participation
-  has_one :user, through: :participation
 
   validates :comment, presence: true
   validates :file, presence: true
